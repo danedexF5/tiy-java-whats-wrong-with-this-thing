cf15217cb3193fa35d13c1360e6a50bf4794f720
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-04-12 14:06:23 -0400

    src

--- src/Charge.java
+++ src/Charge.java
@@ -1,12 +1,12 @@
 
 public class Charge {
 
-     public void main(String[] args){
+     public static void main(String[] args){
 
          boolean nearTeslaCoil = true;
 
          // An electrifying question
-         System.out.println("Is my hair standing on end? " + nearTeslaCoil);
+         System.out.println("Is my hair standing on end?" + nearTeslaCoil);
 
     }
 }
